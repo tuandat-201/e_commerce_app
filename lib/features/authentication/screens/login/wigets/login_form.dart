38fb1a43ae5abd2fe9368b7e5f5060c64d1ef99b
Le Dat <tuandat071101@gmail.com>
2025-04-13 17:40:59 +0700

    Create Navaigation Bar

--- lib/features/authentication/screens/login/wigets/login_form.dart
+++ lib/features/authentication/screens/login/wigets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:e_commerce_app/features/authentication/screens/signup/signup.dart';
+import 'package:e_commerce_app/navigation_menu.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
@@ -71,7 +72,9 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Get.to(() => const NavigationMenu());
+                },
                 child: const Text(
                   TTexts.signIn,
                 ),
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,57 @@
+import 'package:e_commerce_app/features/shop/screens/home.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.slelectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.slelectedIndex.value = index,
+          backgroundColor: dark ? Colors.black : Colors.white,
+          indicatorColor: dark
+              // ignore: deprecated_member_use
+              ? Colors.white.withOpacity(0.2)
+              // ignore: deprecated_member_use
+              : Colors.black.withOpacity(0.1),
+          destinations: [
+            const NavigationDestination(
+                icon: Icon(Iconsax.home), label: 'Home'),
+            const NavigationDestination(
+                icon: Icon(Iconsax.shop), label: 'Store'),
+            const NavigationDestination(
+                icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            const NavigationDestination(
+                icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
+      ),
+      body: Obx(
+        () => controller.screens[controller.slelectedIndex.value],
+      ),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final slelectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.red),
+    Container(color: Colors.amber),
+    Container(color: Colors.blue),
+  ];
+}
